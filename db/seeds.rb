4f02e1987febe592e4f12b90e5b9134ba1622118
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-05 18:18:28 -0500

    half-seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,53 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+require 'faker'
+
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+
+ 
+ # Create Lists
+ 10.times do
+   list = List.create!(
+     user: users.sample,
+     title:  Faker::Lorem.words(5),
+     description:   Faker::Lorem.sentence(2)
+   )
+ end
+
+ lists = List.all
+
+ 
+ # Create Items
+ 25.times do
+   items = Item.create!(
+     list: lists.sample,
+     name: Faker::Lorem.words(5)
+   )
+ end
+
+
+ # Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+
+
+ puts "Seed finished"
+ puts "#{Item.count} lists created"
+ puts "#{List.count} items created"
