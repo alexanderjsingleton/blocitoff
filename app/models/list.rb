befb03fef3f9733ad397a2dfe9d7cbbb54dbd5ea
alexanderjsingleton <alexanderjsingleton@gmail.com>
2015-04-07 11:57:58 -0500

    revised list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,5 +2,5 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :items, dependent: :destroy
 
-  validates :title, length {minimum: 5}, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
 end
