8acd0c25745c4f3a5a70cf692878083ee2b1ad4f
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-15 12:25:32 -0500

    enable action_mailer for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'singleton-blocitoff.heroku.com' }
 end
