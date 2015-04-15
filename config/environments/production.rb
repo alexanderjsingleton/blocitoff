32f6f53019219bf363246736e45acb24c2e69512
Alex Singleton <alexanderjsingleton@gmail.com>
2015-04-15 12:28:21 -0500

    unset production variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'singleton-blocitoff.heroku.com' }
+  # config.action_mailer.default_url_options = { :host => 'singleton-blocitoff.heroku.com' }
 end
